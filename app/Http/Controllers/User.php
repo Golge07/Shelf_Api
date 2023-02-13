771b1c9aa7b0cf80fe8f9da28f1a5a87cad0c861
Eren Bulut <114662337+Golge07@users.noreply.github.com>
2023-02-13 14:38:11 +0300

    kullanici girisi,kayit olma, get/update/delete fonksiyonlari eklendi

---
+++ app/Http/Controllers/User.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use \Illuminate\Support\Str;
+use Illuminate\Support\Facades\URL;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email',
+                'password' => 'required|string|min:4',
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => $validator->errors()->first(),
+                ], 401);
+            }
+            if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+                $token = auth()->user()->createToken('AccessToken')->plainTextToken;
+                return response()->json([
+                    'message' => 'Successfully logged in!',
+                    'token' => $token
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Invalid credentials',
+                ], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong!',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function get_user_by_token(Request $request)
+    {
+        if ($request->user()) {
+            return response()->json([
+                'user' => auth()->user()
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'You are not logged in!',
+            ], 401);
+        }
+    }
+
+    public function get_users_by_permission(Request $request)
+    {
+        if (auth()->user()) {
+            $per = [
+                'str' => substr(auth()->user()->permission, 0),
+                'int' => substr(auth()->user()->permission, 1)
+            ];
+            if ($per['str'] == 'A') {
+                $users = User::all();
+            } else {
+                $users = User::where('permission', 'like', $per['str'] . '%')->where('permission', '>', $per['str'] . $per['int'])->get();
+            }
+            return response()->json([
+                'users' => $users
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'You are not logged in!',
+            ], 401);
+        }
+    }
+
+    public function search_user_by_permission(Request $request)
+    {
+        try {
+            if (auth()->user()) {
+                $per = [
+                    'str' => substr(auth()->user()->permission, 0),
+                    'int' => substr(auth()->user()->permission, 1)
+                ];
+                if ($per['str'] == 'A') {
+                    $users = User::where($request->type, 'like', '%' . $request->value . '%')->get();
+                } else {
+                    $users = User::where($request->type, 'like', '%' . $request->value . '%')->where('permission', 'like', $per['str'] . '%')->where('permission', '>', $per['str'] . $per['int'])->get();
+                }
+                return response()->json([
+                    'users' => $users
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'You are not logged in!',
+                ], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong!',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try {
+            if ($request->user()) {
+                $validator = Validator::make($request->all(), [
+                    'name' => 'required|string',
+                    'email' => 'required|string|email',
+                    'phone' => 'required|string',
+                    'permission' => 'required|string',
+                ]);
+                if ($validator->fails()) {
+                    return response()->json([
+                        'message' => $validator->errors()->first(),
+                    ], 401);
+                }
+                $user = User::where('email', $request->email)->first();
+                if ($user) {
+                    $user->name = $request->name;
+                    $user->phone = $request->phone;
+                    $user->permission = $request->permission;
+                    $user->save();
+                    return response()->json([
+                        'message' => 'Successfully updated user!',
+                        'user' => $user
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'message' => 'User not found!',
+                    ], 401);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'You are not logged in!',
+                ], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong!',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        try {
+            if ($request->user()) {
+                $validator = Validator::make($request->all(), [
+                    'email' => 'required|string|email',
+                ]);
+                if ($validator->fails()) {
+                    return response()->json([
+                        'message' => $validator->errors()->first(),
+                    ], 401);
+                }
+                $user = User::where('email', $request->email)->first();
+                if ($user) {
+                    $user->delete();
+                    return response()->json([
+                        'message' => 'Successfully deleted user!',
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'message' => 'User not found!',
+                    ], 401);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'You are not logged in!',
+                ], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong!',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:4',
+        ]);
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $user->save();
+
+        $token = $user->createToken('Token')->plainTextToken;
+
+        $token = Str::random(60);
+        $domain = URL::to('/');
+        $link = $domain . '/verify/' . $token;
+        $data['user'] = $user;
+        $data['link'] = $link;
+
+        Mail::send('mail.verify', ['data' => $data], function ($message) use ($user) {
+            $message->to($user->email, $user->name)->subject('Verify Mail');
+        });
+
+        return response()->json([
+            'message' => 'Successfully created user!',
+            'token' => $token
+        ], 200);
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique()->primary();
+            $table->string('phone')->unique()->nullable();
+            $table->string('permission')->default('D1');
+            $table->boolean('email_verified')->default(false);
+            $table->boolean('phone_verified')->default(false);
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
